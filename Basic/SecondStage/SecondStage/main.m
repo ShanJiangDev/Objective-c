d22038acadd9bc6f11051e6dfb27c99255743da7
Shan <shanjiang.developer@gmail.com>
2016-04-10 21:28:37 +0200

    rename testString to FirstStage, add SecondStage and start new course

---
+++ Basic/SecondStage/SecondStage/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SecondStage
+//
+//  Created by Shan on 4/10/16.
+//  Copyright (c) 2016 Shan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
